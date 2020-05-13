086ed844a3a47f89024346b20d1003e0b4231f42
guisesterheim <guilherme.sesterheim@gmail.com>
2020-05-12 22:32:47 -0300

    configuring ansible

--- ansible_startup/general-test.sh
+++ ansible_startup/general-test.sh
@@ -1,5 +1,5 @@
 checkStatus=$(curl http://localhost:8080/api/v1/healthCheck)
-checkStatus2=$(sudo docker ps | grep -c LogService)
+checkStatus2=$(sudo docker ps | grep -c LogAnalytics)
 
 if [[ -z "$checkStatus" || $checkStatus2 -eq 0 ]]; then
     echo "Service is unavailable!"
